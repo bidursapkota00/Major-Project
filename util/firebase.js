f132bfbc138ad92dd86e0295e44465420737cbef
bidursapkota00 <bidursapkota00@gmail.com>
2022-10-13 18:57:20 +0545

    firebase and cloudinary

--- util/firebase.js
+++ util/firebase.js
@@ -3,7 +3,7 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.FIREBASE_env.FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
