dffba1501b3256071d1e7d47918879eccc92a6c9
bidursapkota00 <bidursapkota00@gmail.com>
2022-10-14 20:45:07 +0545

    register

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,20 +1,5 @@
 import nextConnect from 'next-connect';
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
+import { db } from '../../util/firebase';
 import { ref, set } from 'firebase/database';
 
 const handler = nextConnect({
@@ -28,21 +13,15 @@ const handler = nextConnect({
 
 handler.post(async (req, res) => {
   const { name, email, password, number, citizenship } = req.body;
-  try {
-    set(ref(db, `/users/1`), {
-      name,
-      email,
-      password,
-      number,
-      citizenship,
-      new: true,
-    });
-    res.status(200).send({ message: `Successfully Registered ${name}` });
-  } catch (error) {
-    res
-      .status(500)
-      .send({ message: `Failed Registered ${name} ${error.message}` });
-  }
+  set(ref(db, `/users/${Date.now()}`), {
+    name,
+    email,
+    password,
+    number,
+    citizenship,
+    new: true,
+  });
+  res.status(200).send({ message: 'Successfully Registered' });
 });
 
 export default handler;
