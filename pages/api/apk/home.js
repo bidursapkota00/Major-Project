64e926a7f0921cbe4bf0eb71839ed49a4370deae
bidursapkota00 <bidursapkota00@gmail.com>
2022-11-11 15:53:41 +0545

    home apk api

---
+++ pages/api/apk/home.js
@@ -0,0 +1,19 @@
+import nextConnect from 'next-connect';
+import { error } from '../../../util/apierr';
+import db from '../../../util/mongodb';
+import { Models } from '../../../modal';
+
+const { Device } = Models;
+
+const handler = nextConnect(error);
+
+handler.post(async (req, res) => {
+  const { device } = req.body;
+  await db.connect();
+  const devicee = await Device.findById(device)
+    .select('user stream total_litre datas')
+    .populate({ path: 'user', select: 'name' });
+  res.send({ message: devicee });
+});
+
+export default handler;
